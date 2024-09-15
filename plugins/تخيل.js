4f61ca82e5f7ee1450b9415d9a060ec1b408c210
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-09-15 14:34:03 +0300

    Update تخيل.js

--- plugins/تخيل.js
+++ plugins/تخيل.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 let processingRequest = false;
 const token = Buffer.from("ZXlKaGJHY2lPaUpTVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnBjM01pT2lKelpXRXRZWEowSWl3aVlYVmtJanBiSW14dloybHVJbDBzSW1WNGNDSTZNVGN4TnpJeU5EWTROQ3dpYVdGMElqb3hOekV5TURRd05qZzBMQ0pxZEdraU9pSXpOVE15Tmprek16UTJNRGcxTURZNU15SXNJbkJoZVd4dllXUWlPbnNpYVdRaU9pSTFOMkV4TW1JMk1URXdObUkwTXpRMk5tWXhaV0V4TWpNMVpqQTBOVFZqTUNJc0ltVnRZV2xzSWpvaWRHWm1abWRtZURnM1FHZHRZV2xzTG1OdmJTSXNJbU55WldGMFpWOWhkQ0k2TVRjd05EY3pOVEUwTURVM01Td2ljM1JoZEhWeklqb3hmWDAuQ1NIemtLV19kcFV0TFRwWF9QWHNqSmxhOVQ2cTVFYnJnNGpZNVZXRW1CYUNROHNWemxUUjNPSFdrRWk4QzZ2RmhPcEJvUGdJMHFtNVgyX2lsdnRSVXZBNHVUUFI3UlJ1OWRZTEZJLXlXSDNCSmY3SVpQSF82bVFDMlJZRk1sOGFDY2JCTVNybFFZb3BEWEhyby0wTjNUUFBuaUtaM1Z0RG1jNGhibUNNaE5XUmZ6OF9pcU9MRnA1bWhlR1pjUXRYZHFtV0g4TklqaF94T042eXBzek5mTVVaUm9TUlZVSy02dVVmV3hLMnpQTW1KOEZpNVAzbl9NUG8tWkdweGI2OVFvWTkwQlphRFRUMmpkUTZsNnoxa2x3ZTBGMTNjelRYbDRTN0xnWnVOaWNGSFB1M0NqNk5DOTVIY0MwRnoweEhjSWFiSGdXUnQ2eG5ZNGZKbUtwN0dB","base64").toString("utf-8");
 
@@ -29,7 +30,7 @@ async function getmodel(modelid) {
 
 const handler = async (m, { conn, args, usedPrefix }) => {
     if (processingRequest) {
-        return await m.reply('طلب آخر قيد المعالجة حاليًا. انتظر من فضلك.');
+        return await m.reply('هناك طلب قيد المعالجة حاليًا. يرجى الانتظار حتى يتم الانتهاء.');
     }
 
     processingRequest = true;
@@ -37,32 +38,37 @@ const handler = async (m, { conn, args, usedPrefix }) => {
     try {
         const text = args.length >= 1 ? args.join(" ") : m.quoted && m.quoted.text || '';
         if (!text.trim()) {
-            await m.reply(`تخيل و انا سأرسل لك ما تخيلته انا بوت السلطان استطيع فعلها نعم لكن اكتب ما تريده باللغه الانجليزيه ! \nمـــثال:\n*.تخيل* girl with hijan  style`);
+            await m.reply(`*اكتب وصف ما تريد تخيله بالإنجليزية للحصول على نتائج دقيقة.*\n*مثال:*\n*.تخيل* girl with hijab style`);
             return;
         }
 
+        // اختيار نموذج عشوائي
         const models = ['68cce2cd1bef6cbb6393876d4790da43', '014004d18d0dcc73a128467096107c69', '038254337d59ef522fdb64268bc28e47', '0c97b277da98f5ff4d92774e0fe69889'];
         const randomModel = models[Math.floor(Math.random() * models.length)];
         const neg_prompt = 'Bad hand, bad face, bad art, bad legs, bad chest, bad body, bad quality';
         const payload = { token, model_id: randomModel, prompt: text, neg_prompt };
 
+        // طلب توليد الصورة
         const preResponse = await axios.post('https://apis-awesome-tofu.koyeb.app/api/seaart', payload);
         const req_id = preResponse.data.req_id;
         const processedImg = await getimg(req_id);
         const chosmodel = await getmodel(randomModel);
         const media = processedImg.url;
         delete processedImg.info.url;
+
         const caption = Object.entries(processedImg.info).map(([key, value]) => `  ○ *${key.toUpperCase()}:* ${value}`).join('\n');
 
-        await conn.sendMessage(m.chat, { image: { url: media }, caption: `https://chat.whatsapp.com/LFFLFVRwTezEIMs3b2hAHC` }, { quoted: m });
+        await conn.sendMessage(m.chat, { image: { url: media }, caption }, { quoted: m });
     } catch (error) {
         console.error(error);
-        await m.reply('حدث خطأ أثناء معالجة طلبك. الرجاء معاودة المحاولة في وقت لاحق.');
+        await m.reply('حدث خطأ أثناء معالجة طلبك. حاول مرة أخرى لاحقًا.');
     } finally {
         processingRequest = false;
     }
 };
+
 handler.help = ["artdraw"];
 handler.tags = ["drawing"];
 handler.command = /^(تخيل)$/i;
+
 export default handler;
