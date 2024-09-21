6cf0941f2606e9b3ce5b88230b91bb43be7dc25d
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-09-21 10:35:51 +0300

    Update zorar.js

--- plugins/zorar.js
+++ plugins/zorar.js
@@ -5,36 +5,32 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
     // Send fake reply message with serial number
     const sn = '*جـاًر تـجـهيز الـقـائـمـه🛰️...*'; // replace with the actual serial number
-    conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', 'مرحبا بك👋🏻, في بوت ابوهايف:♡', 'status@broadcast');
+    conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', 'مرحبا بك👋🏻, في بوت السلطان', 'status@broadcast');
 
     // Prepare the image
-    var joanimiimg = await prepareWAMessageMedia({ image: { url: 'https://telegra.ph/file/9ea7c13e92000839267ab.jpg'' } }, { upload: conn.waUploadToServer });
+    var joanimiimg = await prepareWAMessageMedia({ image: { url: 'https://telegra.ph/file/ec15edb7e6568daafc093.png' } }, { upload: conn.waUploadToServer });
 
     // Create the interactive message with the image
     const interactiveMessage = {
         header: {
-            title: `*﹝❒═════﹝🍷﹞═════❒﹞*\n *اهلا* 👋🏻 『 ${m.pushName} 』 \n *• اسم البوت: بوت باتشيرا*\n *• اسم المطور: ابوهايف:♡*\n *• وَنَجّنَا بِرَحْمَتِكَ مِنَ القوم الكافرين*\n`
+            title: `*╮───────────────╭*\n\n *│• اهلا ${m.pushName}*\n *│• اسم البوت: بوت السلطان*\n *│• اسم المطور: يـوسـف الـسلطان*\n *│• وَنَجّنَا بِرَحْمَتِكَ مِنَ القوم الكافرين*\n`,
+            hasMediaAttachment: true,
+            imageMessage: joanimiimg.imageMessage,
         },
         body: {
-            text: '*افتح القائمة بواسطه الزر*🔘\n*اليك قائمه بقسم الاوامر وقسم المعلومات*📜\n*﹝❒═════﹝🍷﹞═════❒﹞*\n\n> *Copyright© 2024*.'
+            text: ' *`افتح القائمة بواسطه الزر`🔘*\n\n*╯───────────────╰*\n\n',
         },
-        footer: {
-            text: `تم صنع هذا البوت بواسطه ابوهايف:♡ يمنع سب البوت والبوت يعمل فقط في المجموعات وشكرا لك علي استخدام البوت\n\n\n© Bot by Abu Haif`
-        },
-        media: {
-            media: joanimiimg, // Add the prepared image here
-            mediaType: 'image' // Specify the type of media
-        },
+        footer: { text: `تم صنع هذا البوت بواسطه يوسف السلطان يمنع سب البوت والبوت يعمل فقط في المجموعات وشكرا لك علي استخدام البوت \n\n\n© Bot by Youssef Al Soltan`.trim() },
         nativeFlowMessage: {
             buttons: [
                 {
                     name: 'single_select',
                     buttonParamsJson: JSON.stringify({
-                        title: '🍷اخـتر القـسـم🍷',
+                        title: '💫 اخـتر القـسـم 💫',
                         sections: [
                             {
                                 title: 'قسم الاوامر',
-                                highlight_label: 'بوت باتشيرا',
+                                highlight_label: 'بوت السلطان',
                                 rows: [
                                     {
                                         header: 'يعطيك قسم اوامر الجروب🗣️',
@@ -43,7 +39,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
                                         id: '.قسم-الجروبات'
                                     },
                                     {
-                                        header: 'يعطيك قسم اوامر التنزيلات📤',
+                                        header: 'يعطيك قسم اوامر التنزيلات و البحث📤',
                                         title: 'قـسـم الـتنـزيلات📥✬⃝',
                                         description: '',
                                         id: '.قسم-التنزيلات'
@@ -55,10 +51,22 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
                                         id: '.قسم-الترفيه'
                                     },
                                     {
-                                        header: 'يعطيك قسم اوامر التحويل🛠️',
-                                        title: 'قـسـم الـتحـويل🛠️✬⃝',
+                                        header: 'يعطيك قسم اوامر التحويل🃏',
+                                        title: 'قـسـم الـتحـويل🪄✬⃝',
                                         description: '',
                                         id: '.قسم-التحويل'
+                                    },
+                                    {
+                                        header: 'يعطيك قسم اوامر الفعاليات🧸',
+                                        title: 'قـسـم الفـعالـيات🧩✬⃝',
+                                        description: '',
+                                        id: '.قسم-الفعاليات'
+                                    },
+                                    {
+                                        header: 'يعطيك قسم اوامر الذكاء الاصطناعي 🌐',
+                                        title: 'قـسـم الذكاء الاصطناعي🤖✬⃝',
+                                        description: '',
+                                        id: '.قسم-الذكاء'
                                     },
                                     {
                                         header: 'يعطيك قسم اوامر الدين والأسلام👳🏻‍♂️',
@@ -68,21 +76,21 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
                                     },
                                     {
                                         header: 'يعطيك قسم اوامر المطور⚙️',
-                                        title: 'قـسـم الـمـطور🙎🏻✬⃝',
+                                        title: ' قـسـم الـمـطور🙎🏻✬⃝',
                                         description: '',
                                         id: '.قسم-المطور'
                                     },
                                     {
                                         header: 'يعطيك قسم اوامر الألقاب🖊️',
-                                        title: 'قـسـم الألقاب📕✬⃝',
+                                        title: ' قـسـم الألقاب📕✬⃝',
                                         description: '',
-                                        id: '.القاب-الاعضاء'
+                                        id: '.قسم-الألقاب'
                                     },
                                     {
-                                        header: 'يعطيك قـسم كل الاوامر🍷⃝',
-                                        title: 'كل-الاوامر🍷⃝',
+                                        header: 'يعطيك قـسم الالعاب🕹️',
+                                        title: ' قـسم الالعاب🧩✬⃝',
                                         description: '',
-                                        id: '.كل-الاوامر'
+                                        id: '.قسم-الالعاب'
                                     }
                                 ]
                             }
@@ -91,15 +99,15 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
                     messageParamsJson: ''
                 },
                 {
-                    name: 'cta_url',
+                    name: "cta_url",
                     buttonParamsJson: JSON.stringify({
                         display_text: "قنـاة الـواتـساب📣",
-                        url: "https://whatsapp.com/channel/0029VafG0N8I1rclRCFLaL0g",
-                        merchant_url: "https://whatsapp.com/channel/0029VafG0N8I1rclRCFLaL0g"
+                        url: "https://whatsapp.com/channel/0029VaL2bnW0rGiPZq8B5S2M",
+                        merchant_url: "https://whatsapp.com/channel/0029VaL2bnW0rGiPZq8B5S2M"
                     })
                 },
                 {
-                    name: 'cta_url',
+                    name: "cta_url",
                     buttonParamsJson: JSON.stringify({
                         display_text: "مشاهده البوت🎦",
                         url: "https://youtu.be/-XdmFcY3zQI?si=bzJfbQGwjUk-4rZO",
@@ -113,29 +121,41 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
                         sections: [
                             {
                                 title: '📜معلومات عن البوت📜',
-                                highlight_label: 'ابوهايف:♡',
+                                highlight_label: 'By Youssef Al Soltan',
                                 rows: [
                                     {
                                         header: 'صانع البوت👤',
                                         title: 'الـمطور👾',
-                                        description: 'ابوهايف:♡',
+                                        description: '',
                                         id: '.المطور'
                                     },
                                     {
                                         header: 'خصوصيه استخدام البوت❔❕',
                                         title: 'الاسـتخدام📜',
                                         description: '',
-                                        id: '.الاستخدام'
+                                        id: '.قوانين'
                                     },
                                     {
                                         header: 'ابلاغ او ارسال رساله للمطور💭',
                                         title: 'طـلـب ابـلاغ📨',
                                         description: '',
                                         id: '.بلاغ'
+                                        },
+                                       {
+                                        header: 'روابط دعم البوت✔️',
+                                        title: 'الـروابـط💥',
+                                        description: '',
+                                        id: '.قمرين'
                                     },
                                     {
-                                        header: '🍷لتقييم البوت🍷',
-                                        title: '♡تقييم البوت♡',
+                                        header: 'كيفيه ادخال البوت لجروبك🔮',
+                                        title: 'عـروض البـوت❓',
+                                        description: '',
+                                        id: '.عروض'
+                                    },
+                                    {
+                                        header: 'تقيم البوت⭐',
+                                        title: 'طـلب تقـيم🌟',
                                         description: '',
                                         id: '.تقيم'
                                     }
